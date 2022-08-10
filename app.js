adb20ecc09984747c1d3fffa7522b8eb4b3c7683
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2022-08-10 14:31:01 +0100

    async&sync concept & promise

--- app.js
+++ app.js
@@ -1,5 +1,32 @@
-const user = {
-    name : "yassine",
-    id : "0001"
-}
-console.log(user);
+const newPromise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("All done!"), 5000);
+  });
+   
+  // Creating an asynchronous function using an arrow expression and saving it to a the variable asyncFunction. 
+  const asyncFunction = async () => {
+    // Awaiting the promise to resolve and saving the result to the variable finalResult.
+    const finalResult = await newPromise;
+   
+    // Logging the result of the promise to the console
+    console.log(finalResult); // Output: All done!
+  }
+   
+  asyncFunction();
+
+  /*
+        ## Promise .then() .catch()
+  const testLuck = new Promise((resolve, reject) => {
+  if (Math.random() < 0.5) { 
+    resolve('Lucky winner!')
+  } else {
+    reject(new Error('Unlucky!'))
+  }
+});
+ 
+testLuck.then(message => {
+  console.log(message) // Log the resolved value of the Promise
+}).catch(error => {
+  console.error(error) // Log the rejected error of the Promise
+});
+
+  */
