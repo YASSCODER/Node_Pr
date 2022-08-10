6f7cd5cfc32a00206e848ec94339203e2943364c
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2022-08-10 14:57:42 +0100

    synch&asynch timout_function() notions

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const newPromise = new Promise((resolve, reject) => {
-    setTimeout(() => resolve("All done!"), 5000);
-  });
+const showTimeout = () => {
+    // Calling setTimeout() that passes a function that shows an alert after 5 seconds.
+    setTimeout(() => {
+      alert('I only show once after 5 seconds!');
+    }, 5000);
+  };
    
-  // Creating an asynchronous function using an arrow expression and saving it to a the variable asyncFunction. 
-  const asyncFunction = async () => {
-    // Awaiting the promise to resolve and saving the result to the variable finalResult.
-    const finalResult = await newPromise;
+  // Calling the showTimeout() function
+  showTimeout();
+
+
+// const newPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("All done!"), 5000);
+//   });
    
-    // Logging the result of the promise to the console
-    console.log(finalResult); // Output: All done!
-  }
+//   // Creating an asynchronous function using an arrow expression and saving it to a the variable asyncFunction. 
+//   const asyncFunction = async () => {
+//     // Awaiting the promise to resolve and saving the result to the variable finalResult.
+//     const finalResult = await newPromise;
    
-  asyncFunction();
+//     // Logging the result of the promise to the console
+//     console.log(finalResult); // Output: All done!
+//   }
+   
+//   asyncFunction();
 
   /*
         ## Promise .then() .catch()
